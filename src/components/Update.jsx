ea6ed2dde4ed55985f08953ca86ec0c38b5b9708
muna2411 <nurnahar.muna.2411@gmail.com>
2023-11-09 11:56:46 +0600

    complete main.jsx

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -24,7 +24,7 @@ const Update = () => {
         console.log(updated);
 
   
-        fetch(`https://library-management-server-6mvnu6sz2-munas-projects.vercel.app/book/${_id}`, {
+        fetch(`https://library-management-server-p9rejac0f-munas-projects.vercel.app/book/${_id}`, {
             method: 'PUT',
             headers: {
                 'content-type': 'application/json'
